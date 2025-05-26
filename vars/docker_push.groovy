422e59a7d5e3c7923c73ffbf8c63301875f114a7
Pramod R <pramod19workspace@gmail.com>
2025-05-26 23:42:18 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,4 @@
-def call(image,tag,username){
+def call(String image,String tag,String username){
   withCredentials([usernamePassword("credentialsId" : "dockerhub-username",passwordVariable : "password",usernameVariable : "username",)]){
                 sh "docker login -u ${username} -p ${password}"
                 //sh "docker image tag html-docker-demo ${env.username}/html-docker-demo"
